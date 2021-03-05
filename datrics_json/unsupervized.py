1b59415741c2f2b4f62c5071c834095e177ca805
Tetiana Gladkykh <th@datrics.ai>
2021-03-05 09:16:53 +0200

    test

--- datrics_json/unsupervized.py
+++ datrics_json/unsupervized.py
@@ -178,6 +178,7 @@ def deserialize_label_encoder(model_dict):
 def serialize_onehot_encoder(model):
     categories_ = list(map(lambda x: x.tolist(), model.categories_))
     
+
     serialized_model = {
         "meta": "onehot_encoder",
         "params": model.get_params(),
