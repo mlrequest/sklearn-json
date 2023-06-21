aa6767b74958bc347c56baed8c000b4f0af2891e
Andrew <aheldrich14@gmail.com>
2023-06-21 14:09:14 -0400

    Fix random forest

--- sklearn_json/classification.py
+++ sklearn_json/classification.py
@@ -306,39 +306,30 @@ def deserialize_tree(tree_dict, n_features, n_classes, n_outputs):
 
 
 def serialize_decision_tree(model):
-    tree, dtypes = serialize_tree(model.tree_)
-    serialized_model = {
-        'meta': 'decision-tree',
-        'feature_importances_': model.feature_importances_.tolist(),
-        'max_features_': model.max_features_,
-        'n_classes_': int(model.n_classes_),
-        'n_features_': model.n_features_,
-        'n_outputs_': model.n_outputs_,
-        'tree_': tree,
-        'classes_': model.classes_.tolist(),
-        'params': model.get_params()
-    }
-
-
-    tree_dtypes = []
-    for i in range(0, len(dtypes)):
-        tree_dtypes.append(dtypes[i].str)
-
-    serialized_model['tree_']['nodes_dtype'] = tree_dtypes
+    # tree, dtypes = serialize_tree(model.tree_)
+    #print(model.feature_importances_)
+    serialized_model = {'meta': 'decision-tree'}
+    serialized_model.update({k: (v.tolist() if isinstance(v, np.ndarray) else int(v) if isinstance(v, np.int64) else v) for k, v in vars(model).items() })
+    serialized_model['params'] = model.get_params()
+    if hasattr(model, "tree_"):
+        tree, dtypes = serialize_tree(model.tree_)
+        tree_dtypes = []
+        for i in range(0, len(dtypes)):
+            tree_dtypes.append(dtypes[i].str)
+        serialized_model['tree_'] = {'nodes_dtype': tree_dtypes, **tree}
 
     return serialized_model
 
 
 def deserialize_decision_tree(model_dict):
     deserialized_model = DecisionTreeClassifier(**model_dict['params'])
-
     deserialized_model.classes_ = np.array(model_dict['classes_'])
     deserialized_model.max_features_ = model_dict['max_features_']
     deserialized_model.n_classes_ = model_dict['n_classes_']
-    deserialized_model.n_features_ = model_dict['n_features_']
+    deserialized_model.n_features_in_ = model_dict['n_features_in_']
     deserialized_model.n_outputs_ = model_dict['n_outputs_']
 
-    tree = deserialize_tree(model_dict['tree_'], model_dict['n_features_'], model_dict['n_classes_'], model_dict['n_outputs_'])
+    tree = deserialize_tree(model_dict['tree_'], model_dict['n_features_in_'], model_dict['n_classes_'], model_dict['n_outputs_'])
     deserialized_model.tree_ = tree
 
     return deserialized_model
@@ -405,62 +396,39 @@ def deserialize_gradient_boosting(model_dict):
 
 
 def serialize_random_forest(model):
-    serialized_model = {
-        'meta': 'rf',
-        'max_depth': model.max_depth,
-        'min_samples_split': model.min_samples_split,
-        'min_samples_leaf': model.min_samples_leaf,
-        'min_weight_fraction_leaf': model.min_weight_fraction_leaf,
-        'max_features': model.max_features,
-        'max_leaf_nodes': model.max_leaf_nodes,
-        'min_impurity_decrease': model.min_impurity_decrease,
-        'min_impurity_split': model.min_impurity_split,
-        'n_features_': model.n_features_,
-        'n_outputs_': model.n_outputs_,
-        'classes_': model.classes_.tolist(),
-        'estimators_': [serialize_decision_tree(decision_tree) for decision_tree in model.estimators_],
-        'params': model.get_params()
-    }
+    serialized_model = {"meta": "rf"}
+    serialized_model.update({k: (v.tolist() if isinstance(v, np.ndarray) else v) for k, v in vars(model).items()})
+    serialized_model["params"] = model.get_params()
+    serialized_model.pop("estimator")
+    serialized_model.pop("estimator_")
+    #serialized_model['estimator'] = serialize_decision_tree(model.estimator)
+    #serialized_model['estimator_'] = serialized_model['estimator'].copy()
+    serialized_model['estimators_'] = [serialize_decision_tree(decision_tree) for decision_tree in model.estimators_]
 
-    if 'oob_score_' in model.__dict__:
-        serialized_model['oob_score_'] = model.oob_score_
-    if 'oob_decision_function_' in model.__dict__:
-        serialized_model['oob_decision_function_'] = model.oob_decision_function_.tolist()
+    #if 'oob_score_' in model.__dict__:
+    #    serialized_model['oob_score_'] = model.oob_score_
+    #if 'oob_decision_function_' in model.__dict__:
+    #    serialized_model['oob_decision_function_'] = model.oob_decision_function_.tolist()
 
-    if isinstance(model.n_classes_, int):
-        serialized_model['n_classes_'] = model.n_classes_
-    else:
-        serialized_model['n_classes_'] = model.n_classes_.tolist()
+    #if isinstance(model.n_classes_, int):
+    #    serialized_model['n_classes_'] = model.n_classes_
+    #else:
+    #    serialized_model['n_classes_'] = model.n_classes_.tolist()
 
     return serialized_model
 
 
 def deserialize_random_forest(model_dict):
-    model = RandomForestClassifier(**model_dict['params'])
+    model = RandomForestClassifier(**model_dict["params"])
+    attrs = [k for k in model_dict if k not in model_dict["params"]]
+    for attr in attrs:
+        if isinstance(model_dict[attr], list):
+            attr_val = np.array(model_dict[attr])
+        else:
+            attr_val = model_dict[attr]
+        setattr(model, attr, attr_val)
     estimators = [deserialize_decision_tree(decision_tree) for decision_tree in model_dict['estimators_']]
     model.estimators_ = np.array(estimators)
-
-    model.classes_ = np.array(model_dict['classes_'])
-    model.n_features_ = model_dict['n_features_']
-    model.n_outputs_ = model_dict['n_outputs_']
-    model.max_depth = model_dict['max_depth']
-    model.min_samples_split = model_dict['min_samples_split']
-    model.min_samples_leaf = model_dict['min_samples_leaf']
-    model.min_weight_fraction_leaf = model_dict['min_weight_fraction_leaf']
-    model.max_features = model_dict['max_features']
-    model.max_leaf_nodes = model_dict['max_leaf_nodes']
-    model.min_impurity_decrease = model_dict['min_impurity_decrease']
-    model.min_impurity_split = model_dict['min_impurity_split']
-
-    if 'oob_score_' in model_dict:
-        model.oob_score_ = model_dict['oob_score_']
-    if 'oob_decision_function_' in model_dict:
-        model.oob_decision_function_ = model_dict['oob_decision_function_']
-
-    if isinstance(model_dict['n_classes_'], list):
-        model.n_classes_ = np.array(model_dict['n_classes_'])
-    else:
-        model.n_classes_ = model_dict['n_classes_']
 
     return model
 
